82a2e80e1ace454aa42937627a2974e8490c453c
Elin <es.fredriksson@gmail.com>
2025-03-22 15:43:57 +0100

    update text, images and udpate design

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navBar = document.querySelector('.navbar');
+// let menuIcon = document.querySelector('#menu-icon');
+// let navBar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navBar.classList.toggle('active')
-}
-
-
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x')
+//     navBar.classList.toggle('active')
+// }
 
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -18,21 +16,20 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
-        }
+        // if(top >= offset && top < offset + height) {
+        //     navLinks.forEach(links => {
+        //         links.classList.remove('active');
+        //         document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        //     })
+        // }
     })
 
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    menuIcon.classList.remove('bx-x')
-    navBar.classList.remove('active')
+    // menuIcon.classList.remove('bx-x')
+    // navBar.classList.remove('active')
 };
-
 
 // scroll reveal
 ScrollReveal({
